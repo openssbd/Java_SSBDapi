4695688e38cd0f154b071554a552aeded87b8f4e
Kenneth Ho <kenneth.ho@riken.jp>
2016-03-30 13:30:13 +0900

    Add comment on TestSSBD

--- src/TestSSBD.java
+++ src/TestSSBD.java
@@ -8,17 +8,24 @@ public class TestSSBD {
         SSBD q = new SSBD();
         String test1=null, test2 = null, test3=null, test4=null;
         try {
+            // searches SSBD on meta_data API for address which contains the word osaka
             test1 = q.meta_data("address", "osaka");
-            test2 = q.scale("bdml__bdml_ID", "d15115");
-            test3 = q.data("bdmlUUID", "2475d");
+
+            // searches SSBD on data API for bdmlID which contains the phrase 2475d
+            test2 = q.data("bdmlUUID", "2475d");
+
+            // searches SSBD on scale API for bdmlID which contains the phrase d15115
+            test3 = q.scale("bdml__bdml_ID", "d15115");
+
+            // searches SSBD on coordsXYZ API for bdmlID which contains the phrase 247 and timepoint 10, offset 0 and limit to 20 data set
             test4 = q.coordsXYZ("247", 10, 0, 20);
         } catch (BadSSBDException e) {
             e.printStackTrace();
         }
-        System.out.println("Main test1: "+test1);
-        System.out.println("Main test2: "+test2);
-        System.out.println("Main test3: "+test3);
-        System.out.println("Main test4: "+test4);
+        System.out.println("SSBD searching using meta_data for address which contains osaka : "+test1);
+        System.out.println("SSBD searching using data for bdmlID which contains the phrase 2475d : "+test2);
+        System.out.println("SSBD searching using scale for bdmlID which contains the phrase d15115 : "+test3);
+        System.out.println("SSBD searching using coordsXYZ for bdmlID which contains the phrase 247 and at timepoint 10, offset 0 and limit to 20 dtaset: "+test4);
 
     }
 } // end Main
